dc971fbf282159f7f2561c279cb10f8937dbf7e9
Maria Souto <maria.souto@dci-student.org>
2025-02-17 14:12:21 +0100

    saving changes again

--- my-react-app/src/components/About.jsx
+++ my-react-app/src/components/About.jsx
@@ -1,23 +1,25 @@
-import 'react';
-import './About.css';
+import "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <section className="about-container">
-            <h1>About Me</h1>
-            <div className="about-content">
-                <img 
-                    src="https://via.placeholder.com/300" 
-                    alt="About me" 
-                    className="about-image" 
-                />
-                <p>
-                    Hello! I&apos;m a passionate Front End Developer who loves creating beautiful and functional web experiences.
-                    I specialize in React, CSS, and modern web technologies.
-                </p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="about-container">
+      <h1>About Me</h1>
+      <div className="about-content">
+        <img
+          src="./src/pictures/Maria-profile.png"
+          alt="About me"
+          className="about-image"
+        />
+        <p>
+          &quot;Hi! I’m Maria from Spain and currently living in Germany. I’m on an
+          exciting journey of learning coding, diving into the world of tech and
+          development. It&apos;s been an amazing experience combining my passion for
+          problem-solving with the new skills I&apos;m gaining along the way!&quot;
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
